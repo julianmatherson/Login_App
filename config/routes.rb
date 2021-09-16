3abb934dd5ff553dce38a66f760ad3ff93a01467
Julian Matherson <jjdavisfinancial@gmail.com>
2021-09-16 14:14:53 -0400

    fixing stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :installs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
- 'home#index'
-=======
   devise_for :users
   root to: 'home#index'
->>>>>>> devise
 end
